0003eef918a46e7cb29559403761d301c3c113e5
JackMcCrack <jack@charon.lan>
2022-12-04 14:51:48 +0900

    day04 done

---
+++ day04/task1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def read_input(filename):
+    num = []
+    lines=open(filename, "r")
+    for l in lines:
+        a,b = l.strip().split(',')
+        a = a.split('-')
+        b = b.split('-')
+        a = [int(a[0]),int(a[1])]
+        b = [int(b[0]),int(b[1])]
+        num.append([a,b])
+    return num
+
+if __name__ == '__main__':
+    n = read_input("input")
+    x = 0
+    for a,b in n:
+        if a[0] <= b[0] and a[1] >= b[1] or a[0] >= b[0] and a[1] <= b[1]:
+            x = x + 1
+            print(a,b)
+    print(x)
