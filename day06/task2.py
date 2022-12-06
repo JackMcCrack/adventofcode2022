f6b3c94155add5095987d642dac1acc92ea0b521
JackMcCrack <jack@charon.lan>
2022-12-06 18:15:06 +0900

    task1/2 done

---
+++ day06/task2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def read_input(filename):
+    data = []
+    lines=open(filename, "r")
+    for l in lines:
+        l.strip()
+        data.append(l)
+    return data
+
+if __name__ == '__main__':
+    d = read_input("input")
+    unique_char = 14
+    for l in d:
+        x = 0
+        while x+unique_char < len(l):
+            check = (l[x:x+unique_char])
+            if len(set(check)) == unique_char:
+                print(x+unique_char)
+                break
+            x = x + 1
